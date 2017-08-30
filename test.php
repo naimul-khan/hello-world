8393ba1f782bcabb4bd3bc15449bc03cd0878eab
nk155742 <nk155742@BC.Baruch.Cuny.Edu>
2017-08-30 17:17:47 -0400

    new stuff

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+echo "Hello World!\n";
+
+// some numeric stuff
+$age = 21; 
+$retirement_age = 65; 
+echo "You have " . ($retirement_age - $age) . " years left until you can retire.";
+    
+
+?>
