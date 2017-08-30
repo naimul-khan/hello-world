fc0cb4dca1b51f68d331f7a0ce1dce8d4b77219b
nk155742 <nk155742@BC.Baruch.Cuny.Edu>
2017-08-30 18:03:40 -0400

    saved work, 08/30/2017 6:03 PM

--- test.php
+++ test.php
@@ -1,10 +1,23 @@
 <?php
-echo "Hello World!\n";
+echo "Hello World! <br>";
 
 // some numeric stuff
 $age = 21; 
 $retirement_age = 65; 
-echo "You have " . ($retirement_age - $age) . " years left until you can retire.";
+echo "You have ". ($retirement_age - $age)." years left until you can retire. <br>";
     
+echo "This is a 
+multi-line
+statement <br>";
+
+
+$text = <<<_END
+This will apparently    preserve
+white
+
+space
+_END;
+
+echo nl2br($text);
 
 ?>
